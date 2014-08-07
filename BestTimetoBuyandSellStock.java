58440b95f64b417bc6a38765e7416c7a523b8189
MichelleXinHe <hexinbj1989@gmail.com>
2014-08-06 21:34:36 -0400

    Create BestTimetoBuyandSellStock.java
    Say you have an array for which the ith element is the price of a given stock on day i.
    
    Design an algorithm to find the maximum profit. You may complete as many transactions as you like (ie, buy one and sell one share of the stock multiple times). However, you may not engage in multiple transactions at the same time (ie, you must sell the stock before you buy again).

---
+++ BestTimetoBuyandSellStock.java
@@ -0,0 +1,12 @@
+//greedy algorithm 
+//sum up all the positive differences
+public class Solution {
+    public int maxProfit(int[] prices) {
+        int sum=0;
+        for(int i=1; i<prices.length; i++){
+            int profit=prices[i]-prices[i-1];
+            if(profit>0) sum+=profit;
+        }
+        return sum;
+    }
+}
